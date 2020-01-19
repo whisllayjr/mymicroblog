05dfc5dabdc6175dace0bf410e4150624886237d
whisllay jr <whisllayjr@protonmail.com>
2020-01-19 15:36:25 -0300

    html templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Whisllay'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day here!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'That physician was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
