ecf2f702a25a20fe193663cad9f669d896eba0e1
whisllay jr <whisllayjr@protonmail.com>
2020-01-28 16:38:17 -0300

    implemented pagination support

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['whisllayjr@protonmail.com']
+    POSTS_PER_PAGE = 3
