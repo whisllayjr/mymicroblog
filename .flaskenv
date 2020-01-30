429c3c99837de660d14c4a4b0669ed50071bdca6
whisllay jr <whisllayjr@protonmail.com>
2020-01-30 11:59:01 -0300

    added password reset feature

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,6 @@
 FLASK_APP=microblog.py
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=exploredexplorer@gmail.com
+MAIL_PASSWORD=A37iLazjbbsIta
--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['whisllayjr@protonmail.com']
+    ADMINS = ['exploredexplorer@gmail.com']
     POSTS_PER_PAGE = 25
